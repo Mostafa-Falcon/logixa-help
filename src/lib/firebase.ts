ed7939aaa5cacedae1900eb099a3b799cee8dd2a
Mostafa Falco <mostafa0falcon@gmail.com>
2026-06-05 11:30:30 +0300

    Embed Firebase config directly

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID!,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDrcFJJG6jfDJTuMqJaM6UlgtZsIbbK1UM",
+  authDomain: "logixa-help.firebaseapp.com",
+  projectId: "logixa-help",
+  storageBucket: "logixa-help.firebasestorage.app",
+  messagingSenderId: "56555986008",
+  appId: "1:56555986008:web:ce122cb0a75007497cfe2d",
+  measurementId: "G-9QC9TQRLTW",
 }
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0]
